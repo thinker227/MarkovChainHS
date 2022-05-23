7cdfd52c289c5b55d9424fcddf41c127997ec5fd
thinker227 <thinker0227@gmail.com>
2022-05-23 11:37:00 +0200

    Implement predict and similar

--- src/Markov.hs
+++ src/Markov.hs
@@ -6,10 +6,13 @@ module Markov (
     registerMany,
     fromList,
     predict,
-    predictSequence
+    predictStd,
+    predictSequence,
+    predictSequenceStd
 ) where
 
-import Data.Map ( Map, empty, findWithDefault, insert )
+import Data.Map ( Map, empty, findWithDefault, insert, toList )
+import System.Random ( Random(randomR), RandomGen, newStdGen )
 
 
 
@@ -46,10 +49,38 @@ registerMany _ chain = chain
 fromList :: Ord a => [a] -> Chain a
 fromList xs = registerMany xs Markov.empty
 
--- | Predicts the next value in a chain based on the current value.
-predict :: a -> Chain a -> a
-predict current chain = error "Not implemented"
+getPrediction :: Int -> [(a, Int)] -> Int -> a
+getPrediction target ((val, chance) : xs) accum =
+    let current = accum + chance in
+    if current >= target
+        then val
+        else getPrediction target xs current
+getPrediction _ _ _ = error "List is empty"
+
+-- | Predicts the next value in a chain based on the current value and a `RandomGen`.
+predict :: (Ord a, RandomGen g) => a -> g -> Chain a -> (a, g)
+predict current gen chain = let
+    edges = findWithDefault (error "Node doesn't exist") current chain
+    edgesList = toList edges
+    total = sum $ map snd edgesList
+    (target, gen') = randomR (0, total) gen
+    prediction = getPrediction target edgesList 0
+    in (prediction, gen')
+
+-- | Predicts the next value in a chain based on the current value and a default `RandomGen`.
+predictStd :: (Ord a) => a -> Chain a -> IO a
+predictStd current chain = do
+    gen <- newStdGen
+    return $ fst $ predict current gen chain
 
 -- | Predicts a sequence of values in a chain.
-predictSequence :: a -> Chain a -> [a]
-predictSequence current chain = current : predictSequence (predict current chain) chain
+predictSequence :: (Ord a, RandomGen g) => a -> g -> Chain a -> [a]
+predictSequence current gen chain = let
+    (prediction, gen') = predict current gen chain
+    in current : predictSequence prediction gen' chain
+
+-- | Predicts a sequence of values in a chain using a default `RandomGen`.
+predictSequenceStd :: (Ord a) => a -> Chain a -> IO [a]
+predictSequenceStd current chain = do
+    gen <- newStdGen
+    return $ predictSequence current gen chain
